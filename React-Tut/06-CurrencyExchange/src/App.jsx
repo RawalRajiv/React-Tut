e4873f968a2d9d4b965ce27f2389724e3dead848
Rajeev Rawal <rajivmum@gmail.com>
2024-01-06 21:01:00 +0800

    Added Heler folder and new project for currency convertor

---
+++ React-Tut/06-CurrencyExchange/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='w-full h-screen flex flex-wrap items-center bg-cover bg-no-repeat content-center'>Exchange Rate</div>
+    </>
+  )
+}
+
+export default App
---
+++ React-Tut/06-CurrencyExchange/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(fromCurrency) {
+
+    const [CurrencyData, setCurrencyData]  = useState({})
+
+    useEffect(()=> {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromCurrency}.json`)
+        .then((apiResponse) => apiResponse.json())
+        .then((apiResponseData) => setCurrencyData(apiResponseData[fromCurrency]))
+        
+    }, [fromCurrency]) //useEffect
+    console.log(CurrencyData);
+    return CurrencyData;
+}
+
+export default useCurrencyInfo;
