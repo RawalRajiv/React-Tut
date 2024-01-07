14423dae792c2aa9365da372886e6b835982aed5
Rajeev Rawal <rajivmum@gmail.com>
2024-01-07 16:29:30 +0800

    Added new component

--- React-Tut/07-ReactRouter/src/App.jsx
+++ React-Tut/07-ReactRouter/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/header/header"
 
 function App() {
   
 
   return (
     <>
+      
       <h1 className="bg-gray-400 text-4xl text-center p-7">React Router</h1>
     </>
   )
