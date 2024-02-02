b39e2b7329c44f1fcc6e683df8ea04ddc6dc576e
Rajeev Rawal <rajivmum@gmail.com>
2024-02-03 01:10:33 +0800

    Change in App.jsx file, added button and on click of button calling API to get user data

--- React-Tut/10-HookExample/src/App.jsx
+++ React-Tut/10-HookExample/src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useCallback, useState } from 'react';
 import './App.css'
+import UserContext from './context/UserContext'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [user, setUser] = useState({});
+/**
+ * Asynchronous function to fetch user data from a remote server and set the user data.
+ *
+ * @return {Promise<void>} Promise representing the completion of the function
+ */
+const getUser = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+    const data = await response.json();
+    setUser(data);  
+  } catch (error) {
+    console.error(error);
+  }
+  
+}
+  
+//useCallback(getUser, [])
+console.log(user);
   return (
-    <>
+    <UserContext.Provider value={user}>
       <h1>React Hook Example</h1>
-    </>
+      <button onClick={getUser}>Get User</button>      
+    </UserContext.Provider>
   )
 }
 
